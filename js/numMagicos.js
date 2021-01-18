a7e1ab6d715593936247521d301210c9b70f539e
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-01-18 17:45:38 -0300

    desarrollé la lógica del problea. Falta ajustar DOM y resetear número aleatorio al reiniciar el juego

--- js/numMagicos.js
+++ js/numMagicos.js
@@ -1,27 +1,17 @@
-function generarNumAleatorio() {
-    let numAleatorio = Math.floor(Math.random() * 99);
-    console.log("🚀 Desde función: generarNumAleatorio. Número aleatorio = ", numAleatorio);
-    let boton = document.getElementById("botonComenzar");
-    console.log("🚀 Desde variable botón = ", boton);
-}
-
+// Función para desplegar input al presionar "Comenzar juego"
 function comenzarJuego() {
-    generarNumAleatorio();
     let seccion = document.getElementsByTagName("section");
     if (botonComenzar.innerHTML === "Comenzar juego") {
         seccion[0].innerHTML += `<div class="d-flex justify-content-center mt-5">
         <form>
             <div class="mb-3">
                 <label class="form-label">Ingrese un número</label>
-                <input type="number" class="form-control" id="numeroUsuario">
-                <div id="numberHelp" class="form-text">El número que ingrese debe estar comprendido en el rango de 0 a 99.</div>
+                <input for="numeroUsuario" type="number" class="form-control numeroUsuario" id="numeroUsuario">
+                <div id="numberHelp" class="form-text">El número que ingrese debe estar comprendido en el rango de 1 a 100.</div>
             </div>
             <button type="button" class="btn btn-dark" onclick="adivinar()">Enviar</button>
         </form>
-    </div>
-    <div class="alert alert-primary d-none" role="alert" id="alert">
-            A simple primary alert—check it out!
-        </div>`
+    </div>`
         botonComenzar.innerHTML = "Volver";
     } else if (seccion[0].hasChildNodes() && seccion[0].children.length > 5) {
         seccion[0].removeChild(seccion[0].children[5]);
@@ -29,19 +19,28 @@ function comenzarJuego() {
     }
 }
 
-function adivinar() {
-    console.log("desde la función adivinar");
-    let numUsuario = document.getElementById("numeroUsuario").value;
-    let alerta = document.getElementById("alert");
+// Genero número aleatorio
+let numAleatorio = Math.floor(Math.random() * 100) + 1;
 
-    while (numUsuario != this.numAleatorio) {
-        if (numUsuario < this.numAleatorio) {
-            alerta.innerHTML = "Te quedaste corto! El número es mayor";
-        } else if (numUsuario > this.numAleatorio) {
-            alerta.innerHTML = "Te pasaste! El número es menor";
-        } else {
-            alerta.innerHTML = "Ingresaste un valor incorrecto! Intenta nuevamente.";
-        }
+// Función para adivinar el número
+function adivinar() {
+    console.log("Desde la función adivinar");
+    let numUsuario = Number(numeroUsuario.value);
+    console.log("Número ingresado = " + numUsuario);
+    console.log("Número aleatorio = " + numAleatorio);
+    let alerta = document.getElementById("msjAlerta");
+
+    if (numUsuario === numAleatorio) {
+        console.log("Adivinaste");
+        alerta.className = "alert alert-success text-center";
+        alerta.innerHTML = "¡Adivinaste!";
+    } else if (numUsuario > numAleatorio) {
+        console.log("Te pasaste");
+        alerta.className = "alert alert-danger text-center";
+        alerta.innerHTML = "¡Incorrecto! Te pasaste";
+    } else if (numUsuario < numAleatorio) {
+        alerta.className = "alert alert-danger text-center";
+        alerta.innerHTML = "¡Incorrecto! Te quedaste corto";
+        console.log("Te quedaste corto");
     }
-    alerta.innerHTML = "¡Correcto! El número es " + this.numAleatorio;
 }
